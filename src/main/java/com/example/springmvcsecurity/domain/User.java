85846e9afde3f5ee9fdb4fb08ddd3cf875e6f612
dragonphyo <thiaphyo1995.com>
2020-02-19 21:57:27 +0630

    security

--- src/main/java/com/example/springmvcsecurity/domain/User.java
+++ src/main/java/com/example/springmvcsecurity/domain/User.java
@@ -1,11 +1,17 @@
 package com.example.springmvcsecurity.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -44,8 +50,40 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
--- src/main/java/com/example/springmvcsecurity/security/WebMVCConfig.java
+++ src/main/java/com/example/springmvcsecurity/security/WebMVCConfig.java
@@ -1,28 +1,40 @@
 package com.example.springmvcsecurity.security;
 
+import com.example.springmvcsecurity.service.UserDetailServiceImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @EnableWebSecurity
 public class WebMVCConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailServiceImpl service;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebMVCConfig(UserDetailServiceImpl service, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.service = service;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("kyaw")
-                .password("{noop}kyaw")
-                .roles("ADMIN","USER")
-                .and()
-                .withUser("thaw")
-                .password("{noop}thaw")
-                .roles("USER")
-                .and()
-                .withUser("Thomas")
-                .password("{noop}thomas")
-                .roles("USER");
+//        auth.inMemoryAuthentication()
+////                .withUser("kyaw")
+////                .password("{noop}kyaw")
+////                .roles("ADMIN","USER")
+////                .and()
+////                .withUser("thaw")
+////                .password("{noop}thaw")
+////                .roles("USER")
+////                .and()
+////                .withUser("Thomas")
+////                .password("{noop}thomas")
+////                .roles("USER");
+        auth.userDetailsService(service).passwordEncoder(bCryptPasswordEncoder);
 
     }
 
